62e0525d92b420acce30efa307baa9407d3cf078
PriyankaViltoria <75841413+PriyankaViltoria@users.noreply.github.com>
2021-05-09 15:41:42 +0530

    Portfolio
    My first commit.

---
+++ WebContent/ext.js
@@ -0,0 +1,19 @@
+window.addEventListener("scroll", function() {
+    var header= document.querySelector("header");
+    header.classList.toggle("sticky",window.scrollY > 0);
+});
+
+function toggleMenu(){
+    var menuToggle = document.querySelector(".toggle");
+    var menu = document.querySelector(".menu");
+    menuToggle.classList.toggle("active");
+    menu.classList.toggle("active");
+}
+
+//typing animation script
+	var typed = new Typed(".typing", {
+	strings: ["Learner", "Developer", "Designer"],
+	typeSpeed: 100,
+	backSpeed: 60,
+	loop: true
+});
